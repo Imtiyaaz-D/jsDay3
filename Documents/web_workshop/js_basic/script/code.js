b50515bbcb5ec78e8184757067fdd4767ce63e12
Imtiyaaz-D <imtdamon777@gmail.com>
2023-04-24 16:48:35 +0200

    Its my first time

---
+++ Documents/web_workshop/js_basic/script/code.js
@@ -0,0 +1,20 @@
+
+let numb1 =4
+let exponent =numb1 **numb1
+console.log(`${numb1} ** ${numb1} is ${exponent}`);//256
+
+let numb2 = 8
+console.log(++numb2);
+console.log(numb2);//9
+
+let numb3 = 27
+console.log(--numb3); 
+console.log(numb3);//26
+
+let numb4 = 9
+console.log(++numb4);
+console.log(numb4);//10
+
+let numb5 = 76
+console.log(numb5--);
+console.log(numb5);//75
