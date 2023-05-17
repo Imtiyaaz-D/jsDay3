4bb267d60b6c4f4b648d690f6fa56c79d5aefcca
Imtiyaaz-D <imtdamon777@gmail.com>
2023-05-17 10:03:01 +0200

    wed

---
+++ Documents/web_workshop/javascript_workshop/jsDay3/script/code.js
@@ -0,0 +1,13 @@
+//Day 3 exerices
+// let data = 'you';console.log(typeof(data))string
+let data = 67;console.log(typeof(data))//number(it changed)
+
+let x = 89;console.log(typeof(x));//number
+
+let firstName = 'Imti';console.log(typeof(firstName))//string
+
+let gender = true;console.log(typeof(gender))//boolean
+
+let surname;console.log(typeof(surname))//undefined
+
+let age = null;console.log(typeof(age))//object 
