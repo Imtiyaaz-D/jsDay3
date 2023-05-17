41b3ac4e576a89611d6fed661ae1785564c09e4d
Imtiyaaz-D <imtdamon777@gmail.com>
2023-05-17 16:37:39 +0200

    4

---
+++ Documents/web_workshop/javascript_workshop/jsDay4/script/code.js
@@ -0,0 +1,12 @@
+let person1 = {
+    age: 18,
+    firstName: `Imti`,
+}
+let person2 ={
+    age : 22,
+    firstName: 'Ameriah',
+}
+console.log(person1);
+console.log(person2);
+let people = [person1, person2]
+console.log(people)
